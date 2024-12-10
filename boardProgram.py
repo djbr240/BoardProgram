5d439917f7c54fbae57a2281c1a3e667762ad795
djbr240 <djbr240@uky.edu>
2024-12-10 08:12:54 -0500

    Added dictionaries for cluePanelLED and pieceRFID. For now they have placeholders. Debug.py added for discrete testing and debugging without the need for the pico.

--- boardProgram.py
+++ boardProgram.py
@@ -609,6 +609,53 @@ start_spaces = {
     "Green" : 41
 }
 
+# Dictionary for the character pieces and the RFID links
+# All zeros as a placeholder for now
+pieceRFID = {
+    "Blue": 0,
+    "Purple": 0,
+    "Red": 0,
+    "Yellow": 0,
+    "Pink": 0,
+    "Green": 0,
+}
+
+# The LED position for the clue panels
+cluePanelLED = {
+    #characters
+    "Blue": 0,
+    "Purple": 0,
+    "Red": 0,
+    "Yellow": 0,
+    "Pink": 0,
+    "Green": 0,
+    
+    #toys
+    "trex": 0,
+    "racecar": 0,
+    "teddybear": 0,
+    "mr_potatohead": 0,
+    "xylophone": 0,
+    "ball": 0,
+    
+    #locations
+    "pooltable": 0,
+    "desk": 0,
+    "chair": 0,
+    "piano": 0,
+    "plant": 0,
+    "diningtable": 0,
+    
+    #time
+    "11": 0,
+    "12": 0,
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0
+}
+
 ########################################################################
 
 # Setup process
